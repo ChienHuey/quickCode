d9bd1cdde6ca01f3eed9160f4cb3d8193cd726fb
Chien Huey <chien.huey@aenetworks.com>
2015-10-29 18:06:53 -0400

    Cleaned up code a little bit. Also parameterized the AWS region.

--- setup_subs_for_pagerduty.py
+++ setup_subs_for_pagerduty.py
@@ -1,12 +1,23 @@
+# Script to create SNS subscriptions from existing SNS topics
+#
+# python setup_subs_for_pagerduty.py <aws_access_key> <aws_secret_key> <aws_region>
+#
+# requires bucket in S3 to be created beforehand. Zone files will be zipped up in a file with name
+# YYYY-MM-DD_zone_file_extract.zip.
+#
+# @param    aws_access_key      AWS access key
+# @param    aws_secret_key      AWS secret key
+# @param    aws_region          AWS region where SNS topics are
+
 import boto3
 import re
 import sys
 
-__author__ = 'chhuey'
+__author__ = 'ChienHuey'
 
 aws_access_key = sys.argv[1]
 aws_secret_key = sys.argv[2]
-region_name = "us-east-1"
+region_name = sys.argv[3]
 
 sev1_regex = re.compile(r'sev1$')
 sev2_regex = re.compile(r'sev2$')
@@ -24,6 +35,7 @@ sev1_topics = []
 sev2_topics = []
 
 # Divide up the topics into two lists, one for SEV1 and one for SEV2
+# Creates subscriptions to the PagerDuty webhooks
 for topic in topics['Topics']:
     topic_arn = topic['TopicArn']
     match1 = sev1_regex.search(topic_arn)
@@ -43,9 +55,9 @@ for topic in topics['Topics']:
 
 print sev1_topics
 print sev2_topics
-
 print sns_client.list_subscriptions()
 
+# Auditing section - determines the number of subscriptions there are
 subscriptions = sns_client.list_subscriptions()
 sub_count = 0
 has_more_subs = subscriptions['NextToken']
@@ -59,11 +71,3 @@ while has_more_subs is not None:
         has_more_subs = None
 
 print "Subscription count: {0}".format(sub_count)
-
-
-
-#sns_client.subscribe(Topic='{0}'.)
-
-
-
-#print topics['Topics']
