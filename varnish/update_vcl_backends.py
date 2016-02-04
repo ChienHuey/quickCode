961d60f49b458af6db5d9ad7a4e5bb2693265e02
Chien Huey <chien.huey@aenetworks.com>
2016-02-04 16:11:48 -0500

    Finished the module function for updating the backend metrics values.

--- varnish/update_vcl_backends.py
+++ varnish/update_vcl_backends.py
@@ -2,14 +2,23 @@
 
 import fileinput
 import re
+import sys
 
 __author__ = 'Chien Huey'
 
 
-# function that updates the specified backends based on the directives file provided
-def update_backends(backends_file, directives_file):
+# function that updates the specified backend metrics based on the directives file provided
+#
+# @param    backends_file       .vcl file with backend definitions
+# @param    directives_file     space-separated file that has the metric to be updated (e.g. .first_byte_timeout) and
+#                                the new value for that metric (e.g. 3s)
+#
+def update_backends_metrics(backends_file, directives_file):
     # load directives_file into dictionary
     directives = {}
+    # empty array with the new file lines
+    new_backends_file = []
+
     with open(directives_file, "r") as text:
         for line in text:
             metric, value = line.split()
@@ -19,11 +28,20 @@ def update_backends(backends_file, directives_file):
     for vcl_line in fileinput.input(backends_file):
         # loop through the directives dictionary
         for metric in directives:
-            re_pattern = "(\." + metric + ")\s*=\s*(\d+)s"
+            re_pattern = "(\\t*|\\s*)(" + metric + ")(\\t*|\\s*)=\\s*(\\w+);"
             match = re.match(re_pattern, vcl_line)
-            #re.sub()
+            if match:
+                old_value = match.group(match.lastindex)
+                new_value = directives[metric]
+                vcl_line = vcl_line.replace(old_value, new_value)
+                match = None
+        # write line to new backends file array
+        new_backends_file.append(vcl_line)
 
+    # after all lines processed, close input file
+    fileinput.close()
 
-update_backends("./sample_backend.vcl", "directives.txt")
-
-regex = "(\t*|\s*)(\.first_byte_timeout)(\t*|\s*)=\s*\w+;"
+    # overwrite the file with the updated lines from the array buffer
+    with open(backends_file, "w") as text:
+        for line in new_backends_file:
+            text.write(line)
